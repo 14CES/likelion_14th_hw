d7e68d6beff543754d9586236dc29fbd0b05d3ae
14CES <2025111968@dgu.ac.kr>
2026-04-05 21:42:30 +0900

    Feat: 4조 최은서

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request): 
+    return render(request, 'main/mainpage.html')
+
+def secondpage(request):
+    context={
+        'generation': 14,
+        'info':{
+            'name' : '최은서',
+            'age' : '2006년 04월 12일',
+            'number' : '010-9158-3643'
+        }
+    }
+    return render(request, 'second/secondpage.html', context)
